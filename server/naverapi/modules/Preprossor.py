9396ccc2f29ff58b371256060b785e4166a953d8
유진선 <75780733+rhdenro@users.noreply.github.com>
2022-04-29 15:39:48 +0900

    모델 함수 결합 및 정제된 데이터셋 추가

--- server/naverapi/modules/Preprossor.py
+++ server/naverapi/modules/Preprossor.py
@@ -2,15 +2,14 @@ from django.http import HttpResponse
 import requests, json
 from django.conf import settings
 import pickle
-
+import service
 def preprosessor(request):
-    fopen = open(r'C:\Users\user\Desktop\data.txt', 'r')
+    service.excel_to_txt()
+    fopen = open(r'.\asset\data.txt', 'r')
     i=0
     temp_list=[]
     line = fopen.readlines()
     while True:
-        print(i)
-
         if not line: break
         client_id = getattr(settings, 'NAVER_ID')
         client_secret = getattr(settings, 'NAVER_SECRET')
@@ -21,22 +20,24 @@ def preprosessor(request):
             "Content-Type": "application/json"
         }
         data = {
-            'content': line[7001+i]
+            'content': line[i]
         }
         r = requests.post(url, data=json.dumps(data), headers=header)
         k = HttpResponse(r)
         temp_list.append(k.getvalue().decode('utf-8'))
         i+=1
-        if i==133 or 7000+i==len(temp_list)-2:
+        if i==len(temp_list)-2:
             print(temp_list[-1])
             break
     fopen.close()
-    with  open(r"C:\Users\user\Desktop\data24.pickle",'wb') as fs:
+    with  open(r".\asset\clova_data_dump.pickle",'wb') as fs:
         pickle.dump(temp_list, fs)
-    # 처리결과 리턴 필요(not RESPONSE)
+    service.pickle_find_highlight()
+    df=pd.read_pickle(r'.\asset\final_data_pickle')
+    service.all_new_weight(df)
     return HttpResponse(i)
 
-def service(request):
+def sentiment_analysis(request):
     client_id = getattr(settings, 'NAVER_ID')
     client_secret = getattr(settings, 'NAVER_SECRET')
     url = "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze"
@@ -46,8 +47,14 @@ def service(request):
         "Content-Type": "application/json"
     }
     data = {
-        'content': "싸늘하다. 가슴에 비수가 날아와 꽂힌다." #request 값으로 변경 예정
+        'content': request.body #request 값으로 변경 예정
     }
     r = requests.post(url, data=json.dumps(data), headers=header)
+    k = HttpResponse(r)
+    temp=[k.getvalue().decode('utf-8')]
+    with  open(r".\asset\clova_data_dump.pickle",'wb') as fs:
+        pickle.dump(temp, fs)
+    service.pickle_find_highlight()
+    df=pd.read_pickle(r'.\asset\score.pickle')
 
     return HttpResponse(r)
